b1e4dac39533bb4b33db4c32c6064f48adeed983
Piotr B <b.piotr@gmail.com>
2015-11-09 16:18:54 +0100

    Added validation

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,6 @@
 # Country
 class Country < ActiveRecord::Base
   has_many :places
+
+  validates :name, presence: true, length: { maximum: 256 }
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -7,6 +7,9 @@ class Place < ActiveRecord::Base
   delegate :name, to: :country, prefix: true
   delegate :name, to: :city, prefix: true
 
+  validates :name, presence: true, length: { maximum: 256 }
+  validates :country, :city, :name, :begins_at, :ends_at, presence: true
+
   sifter :country_or_city_contains do |string|
     countries.name.matches("%#{string}%") | cities.name.matches("%#{string}%")
   end
